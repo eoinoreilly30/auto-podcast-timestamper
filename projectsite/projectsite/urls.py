c5189e8dd69449c2f1e4d5d263e0409d248a580c
eoin <eoinoreilly30@gmail.com>
2020-06-03 23:51:17 +0100

    add url form

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('timestamper/', include('timestamper.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('timestamper.urls')),
 ]
--- projectsite/timestamper/views.py
+++ projectsite/timestamper/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django import forms
+from projectsite.timestamper import main
 
 
-def index(request):
-    return HttpResponse("test")
+class URLForm(forms.Form):
+    url = forms.CharField(label='URL')
+
+
+def urlform(request):
+    if request.method == 'POST':
+        form = URLForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data["url"]
+
+            return HttpResponse('thanks')
+    else:
+        form = URLForm()
+
+    return render(request, 'url-form.html', {'form': form})
